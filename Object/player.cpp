d76d7c418883d9a13c45ad5a516265ccaedecf34
fee115315 <fko2247088@stu.o-hara.ac.jp>
2022-10-21 12:01:57 +0900

    弾の実装

--- Object/player.cpp
+++ Object/player.cpp
@@ -1,22 +1,30 @@
 #include "DxLib.h"
 #include "game.h"
 #include "player.h"
+#include "SceneMain.h"
+
 
 namespace
 {
 	// X方向、Y方向の最大速度
 	constexpr float kSpeedMax = 8.0f;
 	constexpr float kAcc = 0.4f;
+	//ショットの生成間隔（フレーム数）
+	constexpr int kShotInterval = 8;
 }
 
-Player::Player()
+Player::Player() :
+	m_pMain(nullptr),
+	m_handle(-1),
+	m_shotInterval(0),
+	m_pos(),
+	m_vec()
 {
-	m_handle = -1;
 }
 
 Player::~Player()
-{
 	
+{
 }
 
 void Player::init()
@@ -29,6 +37,9 @@ void Player::init()
 
 void Player::update()
 {
+	m_shotInterval--;
+	if (m_shotInterval < 0)m_shotInterval = 0;
+
 	// パッド(もしくはキーボード)からの入力を取得する
 	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	if (padState & PAD_INPUT_UP)
@@ -58,6 +69,15 @@ void Player::update()
 	else
 	{
 		m_vec.x *= 0.9f;
+	}
+	//キー入力処理
+	if (padState & PAD_INPUT_1)
+	{
+		if ((m_pMain)&&(m_shotInterval<=0))
+		{
+			m_pMain->creatShot(m_pos);
+			m_shotInterval = kShotInterval;
+		}
 	}
 	m_pos += m_vec;
 }
---
+++ Object/shot.cpp
@@ -0,0 +1,40 @@
+#include "DxLib.h"
+#include "shot.h"
+#include "game.h"
+
+Shot::Shot() :
+	m_handle(-1),
+	m_isExist(false),
+	m_pos(100.0f, 100.0f),
+	m_vec(8.0f, 0.0f)
+{
+}
+
+Shot::~Shot()
+{
+
+}
+
+void Shot::start(Vec2 pos)
+{
+	m_isExist = true;
+	m_pos = pos;
+}
+
+void Shot::update()
+{
+	if (!m_isExist)return;
+
+	m_pos += m_vec;
+	if (m_pos.x > Game::kScreenWidth)
+	{
+		m_isExist = false;
+	}
+}
+// •\Ž¦
+void Shot::draw()
+{
+	if (!m_isExist)return;
+
+	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+}
